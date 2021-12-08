01042cc5897241257162bab4861bc734bae664b3
Zhang <zhangcx@ad.slh.wisc.edu>
2021-12-07 18:42:18 -0600

    a

--- orientation.py
+++ orientation.py
@@ -21,18 +21,21 @@ def get_template():
     cv2.destroyAllWindows()
 
 # tuning the bgr inrange for edge:
-def ToBNW(image_path, ksize = 5):
+def ToBNW(image_path, fx = 1, ksize = 5):
     image = cv2.imread(image_path)
     # image = cv2.resize(image, dsize = (1224, 918))
-    # image = cv2.resize(image, None, fx = 0.5, fy = 0.5)
+    if fx != 1:
+        image = cv2.resize(image, None, fx = fx, fy = fx)
+
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
     ## mask of green (36,0,0) ~ (70, 255,255), mask o yellow (15,0,0) ~ (36, 255, 255)
-    rgbmask = cv2.inRange(image, (80,0,0), (200, 250, 250))
-    mask = cv2.inRange(hsv, (0,0,0), (90, 240, 240))
+    rgbmask = cv2.inRange(image, (80,50,50), (200, 200, 200))
+    rgbmask = cv2.medianBlur(rgbmask, ksize = 3)
+    hsv0 = cv2.inRange(hsv, (0,0,50), (100, 250, 250))
 
-    rgb1 = np.copy(rgbmask).astype(np.int)
-    hsv1 = np.copy(mask).astype(np.int)
+    rgb1 = np.copy(rgbmask).astype(np.uint8)
+    hsv1 = np.copy(hsv0).astype(np.int)
     hsv2 = np.abs(255 - hsv1)
     # print(rgb1)
     # print(hsv1)
@@ -41,8 +44,8 @@ def ToBNW(image_path, ksize = 5):
     combine = np.uint8((hsv2 + rgb1)/2)
     combine[combine < 130] = 0
     # print(combine.dtype)
-    mask = cv2.blur(combine, ksize = (ksize, ksize))
-    edge = cv2.Canny(mask, 20, 200)
+    mask = cv2.medianBlur(combine, ksize = ksize)
+    edge = cv2.Canny(mask, 100, 200)
     # lines = cv2.HoughLinesP(edge, 1, np.pi/180, 80, 30, 10 );
     # print(lines)
     # for i in lines:
@@ -52,37 +55,50 @@ def ToBNW(image_path, ksize = 5):
     #     x2 = i[0][2]
     #     y2 = i[0][3]
     #     cv2.line(edge,(x1,y1),(x2, y2),(0,0,0),2)
-    # cv2.imshow('a', combine)
-    # cv2.imshow('c', image)
-    # cv2.imshow('b', edge)
+    # cv2.imshow('rgb', rgbmask)
+    # cv2.imshow('hsv', np.uint8(hsv2))
+    # cv2.imshow('combine', combine)
+    # cv2.imshow('edge', edge)
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
-    return edge
-
-def contour():
-    image = cv2.imread(image_path)
-    image = cv2.flip(cv2.resize(image, dsize = (1224, 918)), 1)
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    # return edge
+    return combine
 
 # correlate the template
-def rotation():
+def slice():
+    # slice image and remove the ruler thing:
+    path = "../data/CC Lake slice/"
+    Path(path).mkdir(parents=True, exist_ok=True)
+    onlyfiles = [f for f in listdir('../data/CC Lake/') if isfile(join('../data/CC Lake/', f))]
+    # print(onlyfiles)
+    for f in onlyfiles:
+        fname = join('../data/CC Lake/', f)
+        image = cv2.resize(cv2.imread(fname), None, fx = 0.25, fy = 0.25)
+        print(image.shape) # (918, 1224, 3)
+        sliced = image[300:600, 200:1000, :]
+        cv2.imwrite(join(path, f), sliced)
+
+def rotation(fname):
     temp = ToBNW('../data/templates/template_color1.png', ksize = 3)
     # temp = imread()
-    temp = cv2.resize(temp, dsize = None, fx = 0.5, fy = 0.5)
+    # temp = cv2.resize(temp, dsize = None, fx = 0.5, fy = 0.5)
     w, h = temp.shape[0:2]
     print(w, h)
     # cv2.imshow('a', temp)
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
 
-    image1 = cv2.imread('../data/CC Lake BW/Cond3/DSC00839.jpg')
-    # image1 = ToBNW('../data/CC Lake/DSC00839.jpg')
-    image1 = cv2.resize(image1, dsize = (1224, 918))
-    image1 = cv2.resize(image1, dsize = None, fx = 0.5, fy = 0.5)
+    # image1 = cv2.imread('../data/CC Lake BW/Cond3/DSC00839.jpg')
+    image1 = ToBNW(fname, fx = 0.45, ksize = 5)
+    # image1 = cv2.resize(image1, dsize = (1224, 918))
+    # image1 = cv2.resize(image1, dsize = None, fx = 0.25, fy = 0.25)
     W, H = image1.shape[0:2]
     print(W, H)
-    image1_flip = cv2.flip(image1, 0)
-
+    image1_flip = cv2.flip(image1, 1)
+    # cv2.imshow('normal', image1)
+    # cv2.imshow('flip', image1_flip)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
     # start template matching, and look for different angles:
     best_match_score = 0
     res = None
@@ -94,13 +110,13 @@ def rotation():
 
     # scale template as well:
 
-    for s in np.arange(0.8, 1.2, 0.1):
+    for s in np.arange(0.7, 1.3, 0.025):
         # print(s)
         temp = cv2.resize(temp, None, fx = s, fy = s)
-        for i in range(40):
+        for i in range(36):
         #     # the rotate takes angle in degrees
-            img_rot = rotate(image1, 5 * i, mode = 'constant', reshape = False, cval = 0)
-            img_rotflip = rotate(image1_flip, 5 * i, mode = 'constant', reshape = False, cval = 0)
+            img_rot = rotate(image1, 10 * i, mode = 'constant', reshape = False, cval = 0)
+            img_rotflip = rotate(image1_flip, 10 * i, mode = 'constant', reshape = False, cval = 0)
             # set reshape = true, cval = 255 good for part 4-1,2,4...
             # print(img_rot.dtype, temp.dtype)
             res_rot = cv2.matchTemplate(img_rot, temp, method)
@@ -110,40 +126,63 @@ def rotation():
                 best_match_score = res_rot.max()
                 res = res_rot
                 best_img = img_rot
-                info = ('image_rot', i, s)
+                info = ('rot', i, s)
             elif res_rotflip.max() >= best_match_score:
                 best_match_score = res_rotflip.max()
                 res = res_rotflip
                 best_img = img_rotflip
-                info = ('image_rotflip', i, s)
-    print(res, best_match_score, best_img.shape)
+                info = ('rotflip', i, s)
+    print(best_match_score, best_img.shape)
     print(info)
+
+    # plot best image orientation
+    best = cv2.imread(fname)
+    best = rotate(best, 10 * info[1], mode = 'constant', reshape = False, cval = 0)
+    if info[0] == 'rotflip':
+        best = cv2.flip(best, 1)
+
+    cv2.imshow('best', best)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
     #
     # # temp_w = best_temp.shape[1]
     # # temp_h = best_temp.shape[0]
     # result_cols = W - w + 1
     # result_rows = H - h + 1
     # print(result_cols, result_rows)
-    cv2.imshow('a', best_img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # cv2.imshow('a', best_img)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
 
 from os import listdir
 from os.path import isfile, join
 from pathlib import Path
 
 def AllBNW():
-    path = "../data/CC Lake BW/Cond3"
+    path = "../data/CC Lake BW/Cond1"
     Path(path).mkdir(parents=True, exist_ok=True)
-    onlyfiles = [f for f in listdir('../data/CC Lake/') if isfile(join('../data/CC Lake/', f))]
+    onlyfiles = [f for f in listdir('../data/CC Lake slice/') if isfile(join('../data/CC Lake slice/', f))]
     # print(onlyfiles)
-    for f in onlyfiles:
-        fname = join('../data/CC Lake/', f)
-        output = ToBNW(fname)
-        cv2.imwrite(join(path, f), output)
+    for f in onlyfiles[5:15]:
+        fname = join('../data/CC Lake slice/', f)
+        print(fname)
+        output = ToBNW(fname, fx = 0.5, ksize = 3)
+        # cv2.imwrite(join(path, f), output)
+
+def test():
+    path = "../data/CC Lake BW/Cond1"
+    Path(path).mkdir(parents=True, exist_ok=True)
+    onlyfiles = [f for f in listdir('../data/CC Lake slice/') if isfile(join('../data/CC Lake slice/', f))]
+    # print(onlyfiles)
+    for f in onlyfiles[:20]:
+        fname = join('../data/CC Lake slice/', f)
+        print(fname)
+        output = rotation(fname)
 
 if __name__ == '__main__':
     # get_template()
-    rotation()
+    # rotation('../data/CC Lake slice/DSC00840.jpg')
     # ToBNW('../data/CC Lake/DSC00839.jpg')
     # AllBNW()
+    # slice()
+    test()
---
+++ segmentation.py
@@ -0,0 +1,138 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import skimage.data as data
+import skimage.segmentation as seg
+import skimage.filters as filters
+import skimage.draw as draw
+import skimage.color as color
+
+def image_show(image, nrows=1, ncols=1, cmap='gray'):
+    fig, ax = plt.subplots(nrows=nrows, ncols=ncols, figsize=(14, 14))
+    ax.imshow(image, cmap='gray')
+    ax.axis('off')
+    plt.show()
+    return fig, ax
+
+from skimage import data
+import numpy as np
+import matplotlib.pyplot as plt
+
+image = data.astronaut()
+# plt.imshow(image)
+# plt.show()
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage.color import rgb2gray
+from skimage import data
+from skimage.filters import gaussian
+from skimage.segmentation import active_contour
+# image_gray = color.rgb2gray(image)
+image_gray = cv2.imread('../data/CC Lake/DSC00840.jpg', 0)
+image_gray = cv2.resize(image_gray, None, fx = 0.125, fy = 0.125)
+# image_show(image_gray)
+print(image_gray.shape)
+m, n = image_gray.shape
+
+s = np.linspace(0, 2*np.pi, 2000)
+r = 215 + 60*np.sin(s)
+c = 250 + 150*np.cos(s)
+init = np.array([r, c]).T
+
+snake = active_contour(gaussian(image_gray, 3, preserve_range=False),
+                       init, alpha=0.055, beta=5, gamma=0.01)
+#
+fig, ax = plt.subplots(figsize=(7, 7))
+ax.imshow(image_gray, cmap=plt.cm.gray)
+ax.plot(init[:, 1], init[:, 0], '--r', lw=3)
+ax.plot(snake[:, 1], snake[:, 0], '-b', lw=3)
+ax.set_xticks([]), ax.set_yticks([])
+ax.axis([0, image_gray.shape[1], image_gray.shape[0], 0])
+
+plt.show()
+
+
+# cv2.imshow('a', image)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+#
+# snake = seg.active_contour(image_gray, points, alpha=0.06, beta=0.3)
+# # print(snake)
+# for i in snake:
+#     # print(i)
+#     image_out = cv2.circle(image_gray, (int(i[0]), int(i[1])), radius=1, color=(0), thickness=-1)
+
+# import numpy as np
+# import matplotlib.pyplot as plt
+#
+# from skimage.segmentation import random_walker
+# from skimage.data import binary_blobs
+# from skimage.exposure import rescale_intensity
+# import skimage
+#
+# rng = np.random.default_rng()
+#
+# # Generate noisy synthetic data
+# data = skimage.img_as_float(binary_blobs(length=128, seed=1))
+# sigma = 0.35
+# data += rng.normal(loc=0, scale=sigma, size=data.shape)
+# data = rescale_intensity(data, in_range=(-sigma, 1 + sigma),
+#                          out_range=(-1, 1))
+#
+# # The range of the binary image spans over (-1, 1).
+# # We choose the hottest and the coldest pixels as markers.
+# markers = np.zeros(data.shape, dtype=np.uint)
+# markers[data < -0.95] = 1
+# markers[data > 0.95] = 2
+#
+# # Run random walker algorithm
+# labels = random_walker(data, markers, beta=10, mode='bf')
+#
+# # Plot results
+# fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(8, 3.2),
+#                                     sharex=True, sharey=True)
+# ax1.imshow(data, cmap='gray')
+# ax1.axis('off')
+# ax1.set_title('Noisy data')
+# ax2.imshow(markers, cmap='gray')
+# ax2.axis('off')
+# ax2.set_title('Markers')
+# ax3.imshow(labels, cmap='gray')
+# ax3.axis('off')
+# ax3.set_title('Segmentation')
+#
+# fig.tight_layout()
+# plt.show()
+#
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from skimage.color import rgb2gray
+# from skimage import data
+# from skimage.filters import gaussian
+# from skimage.segmentation import active_contour
+#
+#
+# img = data.astronaut()
+# img = rgb2gray(img)
+#
+# s = np.linspace(0, 2*np.pi, 400)
+# r = 100 + 100*np.sin(s)
+# c = 220 + 100*np.cos(s)
+# init = np.array([r, c]).T
+#
+# snake = active_contour(gaussian(img, 3, preserve_range=False),
+#                        init, alpha=0.015, beta=10, gamma=0.001)
+#
+# fig, ax = plt.subplots(figsize=(7, 7))
+# ax.imshow(img, cmap=plt.cm.gray)
+# ax.plot(init[:, 1], init[:, 0], '--r', lw=3)
+# ax.plot(snake[:, 1], snake[:, 0], '-b', lw=3)
+# ax.set_xticks([]), ax.set_yticks([])
+# ax.axis([0, img.shape[1], img.shape[0], 0])
+#
+# plt.show()
+
+# fig, ax = image_show(image)
+# ax.plot(points[:, 0], points[:, 1], '--r', lw=3)
+# ax.plot(snake[:, 0], snake[:, 1], '-b', lw=3);
